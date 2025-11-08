16c03bc4dfb411a41178e916f7621aff50214b4a
naruto055 <1935994680@qq.com>
2025-11-08 17:44:19 +0800

    修改实体类的包名

---
+++ apps/user-auth/auth-entity/src/main/java/com/naruto/pinda/authority/dto/auth/UserRoleDTO.java
@@ -0,0 +1,29 @@
+package com.naruto.pinda.authority.dto.auth;
+
+/**
+ * 用户角色DTO
+ */
+
+import com.naruto.pinda.authority.entity.auth.User;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@ApiModel(value = "UserRoleDTO", description = "用户角色DTO")
+public class UserRoleDTO implements Serializable {
+    @ApiModelProperty(value = "用户id")
+    private List<Long> idList;
+    @ApiModelProperty(value = "用户信息")
+    private List<User> userList;
+}
